72bafabe29cb7844bd8224ea1d252cd3d7c5e926
caltenk <cameron.h.altenkirch@gmail.com>
2024-02-12 19:24:00 +0000

    static/fonts gh test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Hello World</h1>
+<div class="color-background"/>
 
+<h1>Cameron Altenkirch</h1>
+
+
+<style>
+
+  @font-face {
+    font-family: 'Fjalla One';
+    font-style: normal;
+    font-weight: 500;
+    src: local(""), url("/fonts/FjallaOne-Regular.ttf") format("truetype");
+  }
+  
+  h1 {
+    color: #2c2c2c;
+    font-family: "Fjalla One";
+  }
+
+  .color-background {
+    background-color: #f5f0f0;
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    z-index: -3;
+  }
+</style>
